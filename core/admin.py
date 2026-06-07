3a30697c5b77021cfb31f94607c9c089d548ae6b
Duncan Appleby <duncan@progressrpg.com>
2026-06-07 18:30:03 +0100

    feat: enhance GameSettings and User admin interfaces with additional fields and filters

--- core/admin.py
+++ core/admin.py
@@ -11,7 +11,38 @@ class ImageAdmin(admin.ModelAdmin):
 
 @admin.register(GameSettings)
 class GameSettingsAdmin(admin.ModelAdmin):
-    list_display = ("free_timer_limit_seconds",)
+    list_display = (
+        "__str__",
+        "free_timer_limit_seconds",
+        "daily_login_base_xp",
+        "daily_login_max_xp",
+        "premium_activity_xp_multiplier",
+        "trial_period_days",
+    )
+    fieldsets = (
+        ("Timer", {"fields": ("free_timer_limit_seconds",)}),
+        (
+            "Daily login XP",
+            {
+                "fields": (
+                    "daily_login_base_xp",
+                    "daily_login_streak_step_xp",
+                    "daily_login_max_xp",
+                )
+            },
+        ),
+        (
+            "Activity XP",
+            {
+                "fields": (
+                    "default_activity_xp_per_second",
+                    "premium_activity_xp_multiplier",
+                    "activity_search_includes_tasks",
+                )
+            },
+        ),
+        ("Stripe", {"fields": ("trial_period_days",)}),
+    )
 
     def has_add_permission(self, request):
         return not GameSettings.objects.exists()
